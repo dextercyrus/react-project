e71efd227ecbffb2b271c008311762721b442f18
BEIJING\cyrusyang <cyrusyang@objectivasoftware.com>
2017-08-04 14:47:03 +0800

    update

--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import '@common/styles/index.scss'
 
+let click = ()=>{
+    console.log(123123);
+    alert(321);
+}
 export default  ({})=>{
-    return <div className="img">hello world</div>
+    return <div className="img" onClick={click}>hello world</div>
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Index from '@common/index';
-
 ReactDom.render(<Index />,document.getElementById("main"));
